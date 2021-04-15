0e51ba32d130f2442c81068e5dffc886c177ad8b
phu vinh hoang <billyhoang11.ca@gmail.com>
2021-04-15 03:58:36 -0400

    search playlist

---
+++ src/app/tab1/music.service.ts
@@ -0,0 +1,113 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecommendArtist } from './RecommendArtist.model';//get model music
+import { map, sample } from 'rxjs/operators'; 
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MusicService {
+  val = 'Teacher';
+  url = '';
+
+
+constructor(private http:HttpClient) { }
+public recommend_artist: RecommendArtist[] = [
+  { 
+      id:'0fauHpmSHwodVYIjTqOGHz',
+      name:'Yiruma',
+      uri:"",
+      image:""
+  },
+  { 
+    id:'43ZHCT0cAZBISjO8DG9PnE',
+    name:'Elvis presley',
+    uri:"",
+    image:""
+},
+{ 
+  id:'06HL4z0CvFAxyc27GXpf02',
+  name:'Taylor Swift',
+  uri:"",
+  image:""
+},
+{ 
+  id:'3fMbdgg4jU18AjLCKBhRSm',
+  name:'Michael Jackson',
+  uri:"",
+  image:""
+}
+ ];
+
+ getSample(){
+  this.url = 'https://xamarinsupport.herokuapp.com/sample';
+  return this.http.get<{result_sample}>(this.url).
+  pipe(
+    map(dbsampleData => {
+      const dbSkills = [];
+      console.log(dbsampleData.result_sample)
+      
+      for (const item in dbsampleData.result_sample){
+        console.log(item)
+          dbSkills.push(new RecommendArtist(dbsampleData.result_sample[item].id, dbsampleData.result_sample[item].name,dbsampleData.result_sample[item].uri,dbsampleData.result_sample[item].image));
+        }
+        console.log(dbSkills  )
+        return dbSkills;
+      }
+      ) 
+       );
+  
+  // return this.http.get<{ [key:string]: RecommendArtist} >(this.url).
+  // pipe();
+  
+} 
+searchPlaylist(keyword:string){
+  this.url = 'https://xamarinsupport.herokuapp.com/search/'+keyword;
+  return this.http.get<{result_sample}>(this.url).
+  pipe(
+    map(dbsampleData => {
+      const dbSkills = [];
+      console.log(dbsampleData.result_sample)
+      
+      for (const item in dbsampleData.result_sample){
+        console.log(item)
+          dbSkills.push(new RecommendArtist(dbsampleData.result_sample[item].id, dbsampleData.result_sample[item].name,dbsampleData.result_sample[item].uri,dbsampleData.result_sample[item].image));
+        }
+        console.log(dbSkills  )
+        return dbSkills;
+      }
+      ) 
+       );
+  
+}
+/* fetchSkills(value:string){//book com c 
+  this.url = 'http://api.dataatwork.org/v1/jobs/autocomplete?begins_with=' + value;
+  console.log(this.url);
+  
+  return this.http.get<{[key:string]: Skill}>(this.url).
+  pipe(
+    map(dbsampleData => {
+            const dbsampleData = [];
+            for (const item in dbsampleData){
+                dbsampleData.push(new Skill(dbsampleData.result_sample[item].uuid, dbsampleData.result_sample[item].normalized_job_title));
+              }
+              console.log(dbsampleData  )
+              return dbsampleData;
+            }
+            ) 
+             );
+} */
+
+
+/* getOneSkill(id: string) {
+  return this.http.get<Skill>('http://api.dataatwork.org/v1/jobs/' + id);
+
+}
+
+getCountries(){
+  return this.http.get<Country[]>('https://restcountries.eu/rest/v2/all');
+
+} */
+}
+
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
-// import { HomeService } from '../home/home.service';
+import { MusicService } from './music.service';
+import { RecommendArtist } from './RecommendArtist.model';
 // import { Item } from '../models/item.model';
-import {NgForm} from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 
 import { Router } from '@angular/router';
+import {NgForm} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+//external link
+//import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -13,14 +18,59 @@ import { Router } from '@angular/router';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  form: FormGroup
+  constructor(private routes:Router,public alertController: AlertController,private musicService: MusicService) {}
+  isLoading = false;
+  sampleArtist : RecommendArtist[];
+  resultPlaylist : RecommendArtist[];
+  test:RecommendArtist
 
-  constructor(private routes:Router,public alertController: AlertController) {}
-  async onSubmit(anyform:NgForm){
-    const alert = await this.alertController.create({
-      header: 'Success',
-      message: 'success',
-      buttons: ['Ok']
+  ngOnInit(){
+    this.form = new FormGroup({
+      playlistName : new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
     });
-    await alert.present();
+
+   /* this.musicService.getSample().subscribe(
+    (sampleData)=>{
+      this.isLoading = false;
+      this.sampleArtist = sampleData;
+      
+    }); */
+    
+}
+  async onSubmit(anyform:NgForm){
+    if(this.form.invalid){
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Playlist name must not be empty',
+        buttons: ['Ok']
+      });
+      await alert.present();
+    } else {
+      //search
+      this.musicService.searchPlaylist(anyform.value.playlistName).subscribe(
+        (sampleData)=>{
+          this.isLoading = false;
+          this.resultPlaylist = sampleData;
+          
+        });
+    }
+    
   }
+  spotify(uri:string){
+    //open
+    console.log(`run spotify`)
+    console.log(uri)
+    //const browser = this.iab.create('https://ionicframework.com/');
+  //   browser.on('loadstop').subscribe(event => {
+  //     browser.insertCSS({ code: "body{color: red;" });
+  //  });
+   
+  //  browser.close();
+  window.open(uri, '_system');
+  }
+
 }
